ca823b979067a13e559ce73f39d02de775b00df3
Boya Pushpalatha <60311622+Pushpalatha76@users.noreply.github.com>
2021-10-14 21:57:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Filehandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Filehandling!");
+            FileStream fs = new FileStream("C:\\Users\\pushpalatha.boya\\OneDrive - Trianz\\Desktop\\pushpa.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            System.Console.WriteLine("We can Read and Display from file");
+            string str = sr.ReadLine();
+            while(str!=null)
+            {
+                System.Console.WriteLine(str);
+                str = sr.ReadLine();
+
+            }
+            
+
+
+
+        }
+    }
+}
